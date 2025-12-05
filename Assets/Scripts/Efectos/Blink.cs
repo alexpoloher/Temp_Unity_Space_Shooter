3b9437f23949a51d0e58259aa0bef35ac2431073
alexpoloher <alexpoloher@gmail.com>
2025-12-05 17:14:37 +0100

    a

--- Assets/Scripts/Efectos/Blink.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Blink : MonoBehaviour
-{
-    [SerializeField] public Material original; //Material inicial del jugador
-    [SerializeField] public Material blink;
-}
